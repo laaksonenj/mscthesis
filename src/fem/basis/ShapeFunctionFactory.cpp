546fe34fe3d27c5bc60296705e55919e3762c1af
Joonas Laaksonen <laaksonen.joonas@hotmail.com>
2024-02-25 17:16:03 +0000

    Add precomputation functionality to ShapeFunctionFactory

--- src/fem/basis/ShapeFunctionFactory.cpp
+++ src/fem/basis/ShapeFunctionFactory.cpp
@@ -1,11 +1,13 @@
 #include "fem/basis/ShapeFunctionFactory.hpp"
 
-#include <iostream>
 #include <cassert>
+#include <vector>
 
 namespace fem
 {
 ShapeFunctionFactory::ShapeFunctionFactory()
+    : m_numOfPrecomputedQuad(0)
+    , m_numOfPrecomputedTri(0)
 {
     m_legendrePolynomials.emplace(0, Polynomial1D(1));
     m_legendrePolynomials.emplace(1, Polynomial1D("t"));
@@ -36,133 +38,41 @@ const Polynomial2D& ShapeFunctionFactory::getShapeFunctionDerivative(ElementType
     }
 }
 
+void ShapeFunctionFactory::precomputeShapeFunctions(ElementType elementType, uint32_t p)
+{
+    assert(p >= 1);
+    if (elementType == ElementType_Parallelogram)
+    {
+        if (p > m_numOfPrecomputedQuad)
+        {
+            precomputeQuadShapeFunctions(p);
+            m_numOfPrecomputedQuad = p;
+        }
+    }
+    else
+    {
+        if (p > m_numOfPrecomputedTri)
+        {
+            precomputeTriShapeFunctions(p);
+            m_numOfPrecomputedTri = p;
+        }
+    }
+}
+
 const Polynomial2D& ShapeFunctionFactory::getQuadShapeFunction(const ShapeFunctionDescriptor& d) const
 {
-    return std::visit([this](const auto& arg) -> const Polynomial2D& { return this->getQuadShapeFunction(arg); }, d);
-}
-
-const Polynomial2D& ShapeFunctionFactory::getQuadShapeFunction(const NodalShapeFunctionDescriptor& d) const
-{
-    const uint32_t nodeIdx = d.nodeIdx;
-    assert(nodeIdx < 4);
     if (!m_quadShapeFunctions.contains(d))
     {
-        if (nodeIdx == 0)
-        {
-            m_quadShapeFunctions.emplace(d, mpq_class("1/4") * Polynomial2D("1-x") * Polynomial2D("1-y"));
-        }
-        else if (nodeIdx == 1)
-        {
-            m_quadShapeFunctions.emplace(d, mpq_class("1/4") * Polynomial2D("1+x") * Polynomial2D("1-y"));
-        }
-        else if (nodeIdx == 2)
-        {
-            m_quadShapeFunctions.emplace(d, mpq_class("1/4") * Polynomial2D("1+x") * Polynomial2D("1+y"));
-        }
-        else
-        {
-            m_quadShapeFunctions.emplace(d, mpq_class("1/4") * Polynomial2D("1-x") * Polynomial2D("1+y"));
-        }
-    }
-    return m_quadShapeFunctions.at(d);
-}
-
-const Polynomial2D& ShapeFunctionFactory::getQuadShapeFunction(const SideShapeFunctionDescriptor& d) const
-{
-    const uint32_t sideIdx = d.sideIdx;
-    const uint32_t k = d.k;
-    assert(sideIdx < 4);
-    assert(k >= 2);
-    if (!m_quadShapeFunctions.contains(d))
-    {
-        if (sideIdx == 0)
-        {
-            m_quadShapeFunctions.emplace(d, (mpq_class("1/2") * Polynomial2D("1-y")) * getPhi2D(k, 'x'));
-        }
-        else if (sideIdx == 1)
-        {
-            m_quadShapeFunctions.emplace(d, (mpq_class("1/2") * Polynomial2D("1+x")) * getPhi2D(k, 'y'));
-        }
-        else if (sideIdx == 2)
-        {
-            m_quadShapeFunctions.emplace(d, (mpq_class("1/2") * Polynomial2D("1+y")) * compose(getPhi(k), Polynomial2D("-x")));
-        }
-        else
-        {
-            m_quadShapeFunctions.emplace(d, (mpq_class("1/2") * Polynomial2D("1-x")) * compose(getPhi(k), Polynomial2D("-y")));
-        }
-    }
-    return m_quadShapeFunctions.at(d);
-}
-    
-const Polynomial2D& ShapeFunctionFactory::getQuadShapeFunction(const InternalShapeFunctionDescriptor& d) const
-{
-    const uint32_t k = d.k;
-    const uint32_t l = d.l;
-    assert(k >= 2);
-    assert(l >= 2);
-    if (!m_quadShapeFunctions.contains(d))
-    {
-        m_quadShapeFunctions.emplace(d, getPhi2D(k, 'x') * getPhi2D(l, 'y'));
+        m_quadShapeFunctions.emplace(d, computeQuadShapeFunction(d));
     }
     return m_quadShapeFunctions.at(d);
 }
 
 const Polynomial2D& ShapeFunctionFactory::getTriShapeFunction(const ShapeFunctionDescriptor& d) const
 {
-    return std::visit([this](const auto& arg) -> const Polynomial2D& { return this->getTriShapeFunction(arg); }, d);
-}
-
-const Polynomial2D& ShapeFunctionFactory::getTriShapeFunction(const NodalShapeFunctionDescriptor& d) const
-{
-    const uint32_t nodeIdx = d.nodeIdx;
-    assert(nodeIdx < 3);
     if (!m_triShapeFunctions.contains(d))
     {
-        if (nodeIdx == 0)
-        {
-            m_triShapeFunctions.emplace(d, Polynomial2D("1-x-y"));
-        }
-        else if (nodeIdx == 1)
-        {
-            m_triShapeFunctions.emplace(d, Polynomial2D("x"));
-        }
-        else
-        {
-            m_triShapeFunctions.emplace(d, Polynomial2D("y"));
-        }
-    }
-    return m_triShapeFunctions.at(d);
-}
-
-const Polynomial2D& ShapeFunctionFactory::getTriShapeFunction(const SideShapeFunctionDescriptor& d) const
-{
-    const uint32_t sideIdx = d.sideIdx;
-    const uint32_t k = d.k;
-    assert(sideIdx < 3);
-    assert(k >= 2);
-    if (!m_triShapeFunctions.contains(d))
-    {
-        const Polynomial1D& rho = getRho(k);
-        const Polynomial2D& nodal1 = getTriShapeFunction(NodalShapeFunctionDescriptor(sideIdx));
-        const Polynomial2D& nodal2 = getTriShapeFunction(NodalShapeFunctionDescriptor((sideIdx + 1) % 3));
-        m_triShapeFunctions.emplace(d, (nodal1 * nodal2) * compose(rho, nodal2 - nodal1));
-    }
-    return m_triShapeFunctions.at(d);
-}
-
-const Polynomial2D& ShapeFunctionFactory::getTriShapeFunction(const InternalShapeFunctionDescriptor& d) const
-{
-    const uint32_t k = d.k;
-    const uint32_t l = d.l;
-    if (!m_triShapeFunctions.contains(d))
-    {
-        const Polynomial2D& nodal1 = getTriShapeFunction(NodalShapeFunctionDescriptor(0));
-        const Polynomial2D& nodal2 = getTriShapeFunction(NodalShapeFunctionDescriptor(1));
-        const Polynomial2D& nodal3 = getTriShapeFunction(NodalShapeFunctionDescriptor(2));
-        const Polynomial2D& P_k = getShiftedLegendrePolynomial2D(k, 'x');
-        const Polynomial2D& P_l = getShiftedLegendrePolynomial2D(l, 'y');
-        m_triShapeFunctions.emplace(d, ((nodal1 * nodal2 * nodal3) * P_k) * P_l);
+        m_triShapeFunctions.emplace(d, computeTriShapeFunction(d));
     }
     return m_triShapeFunctions.at(d);
 }
@@ -175,7 +85,7 @@ const Polynomial2D& ShapeFunctionFactory::getQuadShapeFunctionDerivative(const S
     }
     if (!m_quadShapeFunctionDerivatives.at(d).contains(variable))
     {
-        m_quadShapeFunctionDerivatives.at(d).emplace(variable, diff(getQuadShapeFunction(d), variable));
+        m_quadShapeFunctionDerivatives.at(d).emplace(variable, computeQuadShapeFunctionDerivative(d, variable));
     }
     return m_quadShapeFunctionDerivatives.at(d).at(variable);
 }
@@ -188,7 +98,7 @@ const Polynomial2D& ShapeFunctionFactory::getTriShapeFunctionDerivative(const Sh
     }
     if (!m_triShapeFunctionDerivatives.at(d).contains(variable))
     {
-        m_triShapeFunctionDerivatives.at(d).emplace(variable, diff(getTriShapeFunction(d), variable));
+        m_triShapeFunctionDerivatives.at(d).emplace(variable, computeTriShapeFunctionDerivative(d, variable));
     }
     return m_triShapeFunctionDerivatives.at(d).at(variable);
 }
@@ -197,11 +107,7 @@ const Polynomial1D& ShapeFunctionFactory::getLegendrePolynomial(uint32_t n) cons
 {
     if (!m_legendrePolynomials.contains(n))
     {
-        const mpq_class c1 = mpq_class(2*n - 1) / mpq_class(n);
-        const mpq_class c2 = mpq_class(n - 1) / mpq_class(n);
-        const Polynomial1D& PmMinus1 = getLegendrePolynomial(n-1);
-        const Polynomial1D& PmMinus2 = getLegendrePolynomial(n-2);
-        m_legendrePolynomials.emplace(n, (c1 * Polynomial1D("t")) * PmMinus1 - c2 * PmMinus2);
+        m_legendrePolynomials.emplace(n, computeLegendrePolynomial(n));
     }
     return m_legendrePolynomials.at(n);
 }
@@ -215,8 +121,7 @@ const Polynomial2D& ShapeFunctionFactory::getShiftedLegendrePolynomial2D(uint32_
     }
     if (!m_shiftedLegendrePolynomials2D.at(n).contains(variable))
     {
-        const Polynomial2D g("2" + std::string{variable} + "-1");
-        m_shiftedLegendrePolynomials2D.at(n).emplace(variable, compose(getLegendrePolynomial(n), g));
+        m_shiftedLegendrePolynomials2D.at(n).emplace(variable, computeShiftedLegendrePolynomial2D(n, variable));
     }
     return m_shiftedLegendrePolynomials2D.at(n).at(variable);
 }
@@ -226,8 +131,7 @@ const Polynomial1D& ShapeFunctionFactory::getPhi(uint32_t k) const
     assert(k >= 2);
     if (!m_phis.contains(k))
     {
-        const mpq_class C = mpq_class((k-1) * k) / mpq_class(2*k - 1);
-        m_phis.emplace(k, C * (getLegendrePolynomial(k) - getLegendrePolynomial(k-2)));
+        m_phis.emplace(k, computePhi(k));
     }
     return m_phis.at(k);
 }
@@ -242,7 +146,7 @@ const Polynomial2D& ShapeFunctionFactory::getPhi2D(uint32_t k, char variable) co
     }
     if (!m_phis2D.at(k).contains(variable))
     {
-        m_phis2D.at(k).emplace(variable, compose(getPhi(k), Polynomial2D(std::string{variable})));
+        m_phis2D.at(k).emplace(variable, computePhi2D(k, variable));
     }
     return m_phis2D.at(k).at(variable);
 }
@@ -252,8 +156,309 @@ const Polynomial1D& ShapeFunctionFactory::getRho(uint32_t k) const
     assert(k >= 2);
     if (!m_rhos.contains(k))
     {
-        m_rhos.emplace(k, -4 * diff(getLegendrePolynomial(k-1)));
+        m_rhos.emplace(k, computeRho(k));
     }
     return m_rhos.at(k);
+}
+
+Polynomial2D ShapeFunctionFactory::computeQuadShapeFunction(const ShapeFunctionDescriptor& d) const
+{
+    return std::visit([this](const auto& arg) -> Polynomial2D { return this->computeQuadShapeFunction(arg); }, d);
+}
+
+Polynomial2D ShapeFunctionFactory::computeQuadShapeFunction(const NodalShapeFunctionDescriptor& d) const
+{
+    assert(d.nodeIdx < 4);
+    if (d.nodeIdx == 0)
+    {
+        return mpq_class("1/4") * Polynomial2D("1-x") * Polynomial2D("1-y");
+    }
+    else if (d.nodeIdx == 1)
+    {
+        return mpq_class("1/4") * Polynomial2D("1+x") * Polynomial2D("1-y");
+    }
+    else if (d.nodeIdx == 2)
+    {
+        return mpq_class("1/4") * Polynomial2D("1+x") * Polynomial2D("1+y");
+    }
+    else
+    {
+        return mpq_class("1/4") * Polynomial2D("1-x") * Polynomial2D("1+y");
+    }
+}
+
+Polynomial2D ShapeFunctionFactory::computeQuadShapeFunction(const SideShapeFunctionDescriptor& d) const
+{
+    assert(d.sideIdx < 4);
+    assert(d.k >= 2);
+    if (d.sideIdx == 0)
+    {
+        return (mpq_class("1/2") * Polynomial2D("1-y")) * getPhi2D(d.k, 'x');
+    }
+    else if (d.sideIdx == 1)
+    {
+        return (mpq_class("1/2") * Polynomial2D("1+x")) * getPhi2D(d.k, 'y');
+    }
+    else if (d.sideIdx == 2)
+    {
+        return (mpq_class("1/2") * Polynomial2D("1+y")) * compose(getPhi(d.k), Polynomial2D("-x"));
+    }
+    else
+    {
+        return (mpq_class("1/2") * Polynomial2D("1-x")) * compose(getPhi(d.k), Polynomial2D("-y"));
+    }
+}
+
+Polynomial2D ShapeFunctionFactory::computeQuadShapeFunction(const InternalShapeFunctionDescriptor& d) const
+{
+    assert(d.k >= 2);
+    assert(d.l >= 2);
+    return getPhi2D(d.k, 'x') * getPhi2D(d.l, 'y');
+}
+
+Polynomial2D ShapeFunctionFactory::computeTriShapeFunction(const ShapeFunctionDescriptor& d) const
+{
+    return std::visit([this](const auto& arg) -> Polynomial2D { return this->computeTriShapeFunction(arg); }, d);
+}
+
+Polynomial2D ShapeFunctionFactory::computeTriShapeFunction(const NodalShapeFunctionDescriptor& d) const
+{
+    assert(d.nodeIdx < 3);
+    if (d.nodeIdx == 0)
+    {
+        return Polynomial2D("1-x-y");
+    }
+    else if (d.nodeIdx == 1)
+    {
+        return Polynomial2D("x");
+    }
+    else
+    {
+        return Polynomial2D("y");
+    }
+}
+
+Polynomial2D ShapeFunctionFactory::computeTriShapeFunction(const SideShapeFunctionDescriptor& d) const
+{
+    assert(d.sideIdx < 3);
+    assert(d.k >= 2);
+    const Polynomial1D& rho = getRho(d.k);
+    const Polynomial2D& nodal1 = getTriShapeFunction(NodalShapeFunctionDescriptor(d.sideIdx));
+    const Polynomial2D& nodal2 = getTriShapeFunction(NodalShapeFunctionDescriptor((d.sideIdx + 1) % 3));
+    return (nodal1 * nodal2) * compose(rho, nodal2 - nodal1);
+}
+
+Polynomial2D ShapeFunctionFactory::computeTriShapeFunction(const InternalShapeFunctionDescriptor& d) const
+{
+    const Polynomial2D& nodal1 = getTriShapeFunction(NodalShapeFunctionDescriptor(0));
+    const Polynomial2D& nodal2 = getTriShapeFunction(NodalShapeFunctionDescriptor(1));
+    const Polynomial2D& nodal3 = getTriShapeFunction(NodalShapeFunctionDescriptor(2));
+    const Polynomial2D& P_k = getShiftedLegendrePolynomial2D(d.k, 'x');
+    const Polynomial2D& P_l = getShiftedLegendrePolynomial2D(d.l, 'y');
+    return ((nodal1 * nodal2 * nodal3) * P_k) * P_l;
+}
+
+Polynomial2D ShapeFunctionFactory::computeQuadShapeFunctionDerivative(const ShapeFunctionDescriptor& d, char variable) const
+{
+    assert(variable == 'x' || variable == 'y');
+    return diff(getQuadShapeFunction(d), variable);
+}
+
+Polynomial2D ShapeFunctionFactory::computeTriShapeFunctionDerivative(const ShapeFunctionDescriptor& d, char variable) const
+{
+    assert(variable == 'x' || variable == 'y');
+    return diff(getTriShapeFunction(d), variable);
+}
+
+Polynomial1D ShapeFunctionFactory::computeLegendrePolynomial(uint32_t n) const
+{
+    const mpq_class c1 = mpq_class(2*n - 1) / mpq_class(n);
+    const mpq_class c2 = mpq_class(n - 1) / mpq_class(n);
+    const Polynomial1D& PmMinus1 = getLegendrePolynomial(n-1);
+    const Polynomial1D& PmMinus2 = getLegendrePolynomial(n-2);
+    return (c1 * Polynomial1D("t")) * PmMinus1 - c2 * PmMinus2;
+}
+
+Polynomial2D ShapeFunctionFactory::computeShiftedLegendrePolynomial2D(uint32_t n, char variable) const
+{
+    assert(variable == 'x' || variable == 'y');
+    const Polynomial2D g("2" + std::string{variable} + "-1");
+    return compose(getLegendrePolynomial(n), g);
+}
+
+Polynomial1D ShapeFunctionFactory::computePhi(uint32_t k) const
+{
+    assert(k >= 2);
+    const mpq_class C = mpq_class((k-1) * k) / mpq_class(2*k - 1);
+    return C * (getLegendrePolynomial(k) - getLegendrePolynomial(k-2));
+}
+
+Polynomial2D ShapeFunctionFactory::computePhi2D(uint32_t k, char variable) const
+{
+    assert(variable == 'x' || variable == 'y');
+    assert(k >= 2);
+    return compose(getPhi(k), Polynomial2D(std::string{variable}));
+}
+
+Polynomial1D ShapeFunctionFactory::computeRho(uint32_t k) const
+{
+    assert(k >= 2);
+    return -4 * diff(getLegendrePolynomial(k-1));
+}
+
+void ShapeFunctionFactory::precomputeQuadShapeFunctions(int p)
+{
+    precomputeLegendrePolynomials(p);
+    precomputePhis(p);
+
+    std::vector<ShapeFunctionDescriptor> descs;
+    for (int nodeIdx = 0; nodeIdx < 4; nodeIdx++)
+    {
+        descs.push_back(NodalShapeFunctionDescriptor(nodeIdx));
+    }
+    for (int sideIdx = 0; sideIdx < 4; sideIdx++)
+    {
+        for (int k = 2; k <= p; k++)
+        {
+            descs.push_back(SideShapeFunctionDescriptor(sideIdx, k));
+        }
+    }
+    for (int k = 2; k <= p; k++)
+    {
+        for (int l = 2; l <= p; l++)
+        {
+            descs.push_back(InternalShapeFunctionDescriptor(k, l));
+        }
+    }
+
+    for (const auto& desc : descs)
+    {
+        m_quadShapeFunctions.emplace(desc, Polynomial2D(0));
+        m_quadShapeFunctionDerivatives.emplace(desc, std::unordered_map<char, Polynomial2D>{});
+    }
+
+    #pragma omp parallel for schedule(static,1)
+    for (int i = 0; i < descs.size(); i++)
+    {
+        const auto& desc = descs.at(i);
+        Polynomial2D shapeFn = computeQuadShapeFunction(desc);
+        Polynomial2D shapeFnDx = diff(shapeFn, 'x');
+        Polynomial2D shapeFnDy = diff(shapeFn, 'y');
+        m_quadShapeFunctions.at(desc) = std::move(shapeFn);
+        m_quadShapeFunctionDerivatives.at(desc).emplace('x', std::move(shapeFnDx));
+        m_quadShapeFunctionDerivatives.at(desc).emplace('y', std::move(shapeFnDy));
+    }
+}
+
+void ShapeFunctionFactory::precomputeTriShapeFunctions(int p)
+{
+    precomputeLegendrePolynomials(p);
+    precomputeShiftedLegendrePolynomials2D(p);
+    precomputeRhos(p);
+
+    /* The nodal shape functions are needed for the side and internal shape functions so they must be computed in advance. */
+    for (int nodeIdx = 0; nodeIdx < 3; nodeIdx++)
+    {
+        const NodalShapeFunctionDescriptor desc(nodeIdx);
+        Polynomial2D shapeFn = computeTriShapeFunction(desc);
+        Polynomial2D shapeFnDx = diff(shapeFn, 'x');
+        Polynomial2D shapeFnDy = diff(shapeFn, 'y');
+        m_triShapeFunctions.emplace(desc, shapeFn);
+        m_triShapeFunctionDerivatives.emplace(desc, std::unordered_map<char, Polynomial2D>{});
+        m_triShapeFunctionDerivatives.at(desc).emplace('x', shapeFnDx);
+        m_triShapeFunctionDerivatives.at(desc).emplace('y', shapeFnDy);
+    }
+
+    std::vector<ShapeFunctionDescriptor> descs;
+    for (int sideIdx = 0; sideIdx < 3; sideIdx++)
+    {
+        for (int k = 2; k <= p; k++)
+        {
+            descs.push_back(SideShapeFunctionDescriptor(sideIdx, k));
+        }
+    }
+    for (int k = 0; k <= p-2; k++)
+    {
+        for (int l = 0; l <= p-2; l++)
+        {
+            descs.push_back(InternalShapeFunctionDescriptor(k, l));
+        }
+    }
+
+    for (const auto& desc : descs)
+    {
+        m_triShapeFunctions.emplace(desc, Polynomial2D(0));
+        m_triShapeFunctionDerivatives.emplace(desc, std::unordered_map<char, Polynomial2D>{});
+    }
+
+    #pragma omp parallel for schedule(static,1)
+    for (int i = 0; i < descs.size(); i++)
+    {
+        const auto& desc = descs.at(i);
+        Polynomial2D shapeFn = computeTriShapeFunction(desc);
+        Polynomial2D shapeFnDx = diff(shapeFn, 'x');
+        Polynomial2D shapeFnDy = diff(shapeFn, 'y');
+        m_triShapeFunctions.at(desc) = std::move(shapeFn);
+        m_triShapeFunctionDerivatives.at(desc).emplace('x', std::move(shapeFnDx));
+        m_triShapeFunctionDerivatives.at(desc).emplace('y', std::move(shapeFnDy));
+    }
+}
+
+void ShapeFunctionFactory::precomputeLegendrePolynomials(int p)
+{
+    const uint32_t currentNum = m_legendrePolynomials.size();
+    for (int n = currentNum; n <= p; n++)
+    {
+        m_legendrePolynomials.emplace(n, computeLegendrePolynomial(n));
+    }
+}
+
+void ShapeFunctionFactory::precomputeShiftedLegendrePolynomials2D(int p)
+{
+    for (int n = 0; n <= p - 2; n++)
+    {
+        m_shiftedLegendrePolynomials2D.emplace(n, std::unordered_map<char, Polynomial2D>{});
+    }
+
+    #pragma omp parallel for schedule(static,1)
+    for (int n = 0; n <= p - 2; n++)
+    {
+        m_shiftedLegendrePolynomials2D.at(n).emplace('x', computeShiftedLegendrePolynomial2D(n, 'x'));
+        m_shiftedLegendrePolynomials2D.at(n).emplace('y', computeShiftedLegendrePolynomial2D(n, 'y'));
+    }
+}
+
+void ShapeFunctionFactory::precomputePhis(int p)
+{
+    for (int k = 2; k <= p; k++)
+    {
+        m_phis.emplace(k, Polynomial1D(0));
+        m_phis2D.emplace(k, std::unordered_map<char, Polynomial2D>{});
+    }
+
+    #pragma omp parallel for schedule(static,1)
+    for (int k = 2; k <= p; k++)
+    {
+        Polynomial1D phi = computePhi(k);
+        Polynomial2D phi_x = compose(phi, Polynomial2D("x"));
+        Polynomial2D phi_y = compose(phi, Polynomial2D("y"));
+        m_phis.at(k) = std::move(phi);
+        m_phis2D.at(k).emplace('x', std::move(phi_x));
+        m_phis2D.at(k).emplace('y', std::move(phi_y));
+    }
+}
+
+void ShapeFunctionFactory::precomputeRhos(int p)
+{
+    for (int k = 2; k <= p; k++)
+    {
+        m_rhos.emplace(k, Polynomial1D(0));
+    }
+
+    #pragma omp parallel for schedule(static,1)
+    for (int k = 2; k <= p; k++)
+    {
+        m_rhos.at(k) = computeRho(k);
+    }
 }
 } // namespace fem
--- src/fem/basis/ShapeFunctionFactory.hpp
+++ src/fem/basis/ShapeFunctionFactory.hpp
@@ -14,19 +14,14 @@ class ShapeFunctionFactory
 public:
     ShapeFunctionFactory();
 
+    void precomputeShapeFunctions(ElementType elementType, uint32_t p);
+
     const Polynomial2D& getShapeFunction(ElementType elementType, const ShapeFunctionDescriptor& descriptor) const;
     const Polynomial2D& getShapeFunctionDerivative(ElementType elementType, const ShapeFunctionDescriptor& descriptor, char variable) const;
 
 private:
     const Polynomial2D& getQuadShapeFunction(const ShapeFunctionDescriptor& d) const;
-    const Polynomial2D& getQuadShapeFunction(const NodalShapeFunctionDescriptor& d) const;
-    const Polynomial2D& getQuadShapeFunction(const SideShapeFunctionDescriptor& d) const;
-    const Polynomial2D& getQuadShapeFunction(const InternalShapeFunctionDescriptor& d) const;
-
     const Polynomial2D& getTriShapeFunction(const ShapeFunctionDescriptor& d) const;
-    const Polynomial2D& getTriShapeFunction(const NodalShapeFunctionDescriptor& d) const;
-    const Polynomial2D& getTriShapeFunction(const SideShapeFunctionDescriptor& d) const;
-    const Polynomial2D& getTriShapeFunction(const InternalShapeFunctionDescriptor& d) const;
 
     const Polynomial2D& getQuadShapeFunctionDerivative(const ShapeFunctionDescriptor& d, char variable) const;
     const Polynomial2D& getTriShapeFunctionDerivative(const ShapeFunctionDescriptor& d, char variable) const;
@@ -36,6 +31,32 @@ private:
     const Polynomial1D& getPhi(uint32_t k) const;
     const Polynomial2D& getPhi2D(uint32_t k, char variable) const;
     const Polynomial1D& getRho(uint32_t k) const;
+
+    Polynomial2D computeQuadShapeFunction(const ShapeFunctionDescriptor& d) const;
+    Polynomial2D computeQuadShapeFunction(const NodalShapeFunctionDescriptor& d) const;
+    Polynomial2D computeQuadShapeFunction(const SideShapeFunctionDescriptor& d) const;
+    Polynomial2D computeQuadShapeFunction(const InternalShapeFunctionDescriptor& d) const;
+
+    Polynomial2D computeTriShapeFunction(const ShapeFunctionDescriptor& d) const;
+    Polynomial2D computeTriShapeFunction(const NodalShapeFunctionDescriptor& d) const;
+    Polynomial2D computeTriShapeFunction(const SideShapeFunctionDescriptor& d) const;
+    Polynomial2D computeTriShapeFunction(const InternalShapeFunctionDescriptor& d) const;
+
+    Polynomial2D computeQuadShapeFunctionDerivative(const ShapeFunctionDescriptor& d, char variable) const;
+    Polynomial2D computeTriShapeFunctionDerivative(const ShapeFunctionDescriptor& d, char variable) const;
+
+    Polynomial1D computeLegendrePolynomial(uint32_t n) const;
+    Polynomial2D computeShiftedLegendrePolynomial2D(uint32_t n, char variable) const;
+    Polynomial1D computePhi(uint32_t k) const;
+    Polynomial2D computePhi2D(uint32_t k, char variable) const;
+    Polynomial1D computeRho(uint32_t k) const;
+
+    void precomputeQuadShapeFunctions(int p);
+    void precomputeTriShapeFunctions(int p);
+    void precomputeLegendrePolynomials(int p);
+    void precomputeShiftedLegendrePolynomials2D(int p);
+    void precomputePhis(int p);
+    void precomputeRhos(int p);
 
 private:
     mutable std::unordered_map<ShapeFunctionDescriptor, Polynomial2D> m_quadShapeFunctions;
@@ -48,5 +69,8 @@ private:
     mutable std::unordered_map<uint32_t, Polynomial1D> m_phis;
     mutable std::unordered_map<uint32_t, std::unordered_map<char, Polynomial2D>> m_phis2D;
     mutable std::unordered_map<uint32_t, Polynomial1D> m_rhos;
+
+    uint32_t m_numOfPrecomputedQuad;
+    uint32_t m_numOfPrecomputedTri;
 };
 } // namespace fem
